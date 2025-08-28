839f17c2e856edb4eaec43145063451d0414a99f
ronlemire2 <ronlemire2@gmail.com>
2025-08-28 08:57:49 -0700

    Initial Commit

---
+++ Layouts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Layouts
+//
+//  Created by Ron Lemire on 8/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Ron Lemire on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
